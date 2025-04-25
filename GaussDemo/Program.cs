b7f621999504249ad06e8f997afb56f197fbbffc
justerzhu <zhuzhen723723@outlook.com>
2025-04-26 00:00:47 +0800

    feature: SHA256验证
    待测试

---
+++ GaussDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using Npgsql;
+
+namespace GaussDemo;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var connString = @"";
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(connString);
+        var dataSource = dataSourceBuilder.Build();
+        var conn = await dataSource.OpenConnectionAsync();
+    }
+}
