11236f56d5134d8d38dd08253bbd86262aca0e0f
justerzhu <zhuzhen723723@outlook.com>
2025-04-26 00:24:50 +0800

    fix: 移除gauss demo

--- GaussDemo/Program.cs
+++ GaussDemo/Program.cs
@@ -6,9 +6,11 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        var connString = @"";
+        var connString = @"Host=120.46.168.209:8000;Username=root;Password=huawei@db~123;Database=gauss-9ffe;PasswordAuthentication=SHA256";
         var dataSourceBuilder = new NpgsqlDataSourceBuilder(connString);
         var dataSource = dataSourceBuilder.Build();
         var conn = await dataSource.OpenConnectionAsync();
+
+
     }
 }
